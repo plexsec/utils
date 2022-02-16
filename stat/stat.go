dcf30b2da921e5c349c63feb7dcc0387b8db0d8a
wfei <wfei@coolnet.io>
2022-02-16 17:55:01 +0800

    Fix bug

--- stat/stat.go
+++ stat/stat.go
@@ -214,7 +214,7 @@ func loop() {
 				sendBuf.Reset()
 
 				for i := 0; i < len(aw.tags); i++ {
-					sendBuf.WriteString(fmt.Sprintf(",t%d=%s", i+1, aw.tags[i]))
+					sendBuf.WriteString(fmt.Sprintf("t%d=%s", i+1, aw.tags[i]))
 				}
 
 				for i := 0; i < len(attrs); i++ {
